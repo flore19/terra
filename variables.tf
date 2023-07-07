1f7acb7c09251053a81f0c73db662ca2f426bb42
flore <florenzeute@yahoo.fr>
2023-07-07 23:05:18 +0200

    terraform

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  default = "default"
+
+}
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+variable "team" {
+  default = "dev"
+
+}
+variable "protocole_type" {
+  default = "email"
+
+}
+variable "endpoint" {
+  default = "ghghghs@gmail.com"
+
+}
